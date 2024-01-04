dc551a167f2de0948deb98352aab225430caf891
JulianMessner <julianmessner.13@gmail.com>
2024-01-04 18:28:38 +0100

    images adjusted

--- models/background-object.class.js
+++ models/background-object.class.js
@@ -1,14 +1,12 @@
 class BackgroundObject extends MoveableObject {
 
     width = 720;
-    height = 400;
+    height = 480;
 
     constructor(imagePath, x, y){
-
-        
         super().loadImage(imagePath);
         this.x = x;
-        this.y = y;
+        this.y = 480 - this.height; //da BGO height 400 hat und unter world.class.js y mit 80 definiert ist, deshalb 480
     }
 
 }
--- models/character.class.js
+++ models/character.class.js
@@ -1,5 +1,9 @@
 class Character extends MoveableObject{
    
+    height = 280;
+    width = 140;
+    y = 155;
+
     constructor(){
         super().loadImage('img/2_character_pepe/2_walk/W-21.png');
     }
