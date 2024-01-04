f3d92aba844d3515e1631884d7068d86352a3631
JulianMessner <julianmessner.13@gmail.com>
2024-01-04 16:38:39 +0100

    first background layer added

--- models/world.class.js
+++ models/world.class.js
@@ -8,6 +8,9 @@ class World {
     clouds = [
       new Cloud()
     ];
+    backgroundObjects = [
+      new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0, 100)
+    ];
     canvas;
     ctx;
 
@@ -20,20 +23,25 @@ class World {
     draw(){
       this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-      this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
-      
-      this.enemies.forEach(enemy => {
-        this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.width, enemy.height);
-      });
-
-      this.clouds.forEach(cloud => {
-        this.ctx.drawImage(cloud.img, cloud.x, cloud.y, cloud.width, cloud.height);
-      });
+      this.addToMap(this.character);
+      this.addObjectsToMap(this.clouds);
+      this.addObjectsToMap(this.enemies);
+      this.addObjectsToMap(this.backgroundObjects);
 
       //draw() wird immer wieder aufgerufen
       let self = this; 
       requestAnimationFrame(function(){
         self.draw();
       });
+    }
+
+    addObjectsToMap(objects){
+      objects.forEach(o => {
+        this.addToMap(o);
+      });
+    }
+
+    addToMap(moveableObjevt) {
+      this.ctx.drawImage(moveableObjevt.img, moveableObjevt.x, moveableObjevt.y, moveableObjevt.width, moveableObjevt.height);
     }
 }
