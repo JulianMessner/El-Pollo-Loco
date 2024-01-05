233786e1236dd60671990da156b555b5097550d6
JulianMessner <julianmessner.13@gmail.com>
2024-01-05 11:47:04 +0100

    character walk images added

--- models/moveable-object.class.js
+++ models/moveable-object.class.js
@@ -4,11 +4,20 @@ class MoveableObject{
     img;
     height = 150;
     width = 100;
+    imageCache = [];
 
     //loadImage('./img/test.png')
     loadImage(path){
         this.img = new Image(); //Image muss nicht neu definiert werden -> hier dasselbe wie this.img = document.getElementById('image') <img id="image" src>
         this.img.src = path;
+    }
+
+    loadImages(array){
+            array.forEach(path => {
+                let img = new Image();
+                img.src = path;
+                this.imageCache[path] = path;
+            });
     }
 
     moveRight(){ //nicht notwendig function davor zu schreiben, da in class dies automatisch als Funktion deklariert wird
