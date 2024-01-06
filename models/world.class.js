7610129464577d6d78a2a5014c7081bdaf5fa29b
JulianMessner <julianmessner.13@gmail.com>
2024-01-06 15:06:17 +0100

    level class added

--- models/world.class.js
+++ models/world.class.js
@@ -1,38 +1,8 @@
 class World {
   character = new Character(); //let wird in Klassen nicht benötigt
-  enemies = [
-    //let wird in Klassen nicht benötigt
-    new Chicken(),
-    new Chicken(),
-    new Chicken(),
-  ];
-  clouds = [new Cloud()];
-  backgroundObjects = [
-    new BackgroundObject("img/5_background/layers/air.png", -719), //-719 steht für x-Koordinate
-    new BackgroundObject("img/5_background/layers/3_third_layer/2.png", -719),
-    new BackgroundObject("img/5_background/layers/2_second_layer/2.png", -719),
-    new BackgroundObject("img/5_background/layers/1_first_layer/2.png", -719),
-
-    new BackgroundObject("img/5_background/layers/air.png", 0),
-    new BackgroundObject("img/5_background/layers/3_third_layer/1.png", 0),
-    new BackgroundObject("img/5_background/layers/2_second_layer/1.png", 0),
-    new BackgroundObject("img/5_background/layers/1_first_layer/1.png", 0),
-
-    new BackgroundObject("img/5_background/layers/air.png", 719),
-    new BackgroundObject("img/5_background/layers/3_third_layer/2.png", 719),
-    new BackgroundObject("img/5_background/layers/2_second_layer/2.png", 719),
-    new BackgroundObject("img/5_background/layers/1_first_layer/2.png", 719),
-
-    new BackgroundObject("img/5_background/layers/air.png", 719*2),
-    new BackgroundObject("img/5_background/layers/3_third_layer/1.png", 719*2),
-    new BackgroundObject("img/5_background/layers/2_second_layer/1.png", 719*2),
-    new BackgroundObject("img/5_background/layers/1_first_layer/1.png", 719*2),
-
-    new BackgroundObject("img/5_background/layers/air.png", 719*3),
-    new BackgroundObject("img/5_background/layers/3_third_layer/2.png", 719*3),
-    new BackgroundObject("img/5_background/layers/2_second_layer/2.png", 719*3),
-    new BackgroundObject("img/5_background/layers/1_first_layer/2.png", 719*3),
-  ];
+  enemies = level1.enemies;
+  clouds = level1.clouds;
+  backgroundObjects = level1.backgroundObjects;
   canvas;
   ctx;
   keyboard;
