90a78570b87a8721524b6bc3945c3786f751ec42
JulianMessner <julianmessner.13@gmail.com>
2024-01-07 17:23:00 +0100

    add status bar

--- models/world.class.js
+++ models/world.class.js
@@ -8,6 +8,7 @@ class World {
   ctx;
   keyboard;
   camera_x = 0;
+  statusBar = new StatusBar();
 
   constructor(canvas, keyboard) {
     this.ctx = canvas.getContext("2d");
@@ -27,6 +28,7 @@ class World {
       this.level.enemies.forEach(enemy => {
         if(this.character.isColliding(enemy)){
           this.character.hit();
+          this.statusBar.setPercentage(this.character.energy); //reduziert Energybar anhand von Kollision
         }
       });
     }, 200);
@@ -36,8 +38,13 @@ class World {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
     this.ctx.translate(this.camera_x, 0);
-
     this.addObjectsToMap(this.level.backgroundObjects);
+
+    this.ctx.translate(-this.camera_x, 0); // Back
+    // ------ Space for fixed objects -----
+    this.addToMap(this.statusBar);
+    this.ctx.translate(this.camera_x, 0); // Forward
+
     this.addToMap(this.character);
     this.addObjectsToMap(this.level.enemies);
     this.addObjectsToMap(this.level.clouds);
