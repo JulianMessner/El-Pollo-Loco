b474d14bbaa7093c96d4bb3e539e8fecc4c38de0
JulianMessner <julianmessner.13@gmail.com>
2024-02-05 18:41:33 +0100

    small chicken added

---
+++ models/chicken-small.class.js
@@ -0,0 +1,36 @@
+class ChickenSmall extends MoveableObject {
+    height = 60;
+    width = 60;
+  
+    CHICKEN_SMALL_WALKING = [
+      "img/3_enemies_chicken/chicken_small/1_walk/1_w.png",
+      "img/3_enemies_chicken/chicken_small/1_walk/2_w.png",
+      "img/3_enemies_chicken/chicken_small/1_walk/3_w.png",
+    ];
+  
+    CHICKEN_SMALL_DEAD = [
+      "img/3_enemies_chicken/chicken_small/2_dead/dead.png",
+    ];
+  
+    constructor() {
+      super().loadImage("img/3_enemies_chicken/chicken_small/1_walk/1_w.png");
+      this.loadImages(this.CHICKEN_SMALL_WALKING);
+      this.loadImages(this.CHICKEN_SMALL_DEAD);
+      this.x = 200 + Math.random() * 2000;
+      this.y = 370;
+      this.speed = 0.15 + Math.random() * 0.25;
+      this.animate();
+    }
+  
+    animate() {
+      setInterval(() => {
+        this.x -= this.speed;
+      }, 1000 / 60); //60x pro Sekunde wird diese Funktion ausgeführt
+  
+      this.moveLeft();
+  
+      setInterval(() => {
+        this.playAnimation(this.CHICKEN_SMALL_WALKING);
+      }, 100);
+    }
+  }
